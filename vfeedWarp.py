7df524ca0edb8a6431b0d44d4844a920a29f90eb
Sai Uday Shankar Korlimarla <skorlimarla@unomaha.edu>
2015-07-28 18:53:03 -0500

    vfeed Model and search

---
+++ vfeedWarp.py
@@ -0,0 +1,64 @@
+from vfeedModel import nvd_db, cve_cpe, cve_cwe, map_cve_exploitdb
+
+# Take user input here and search
+def processInput(userinput):
+    user_inp = userinput
+        
+    
+# Takes cpe and gives CVEs - Product to Vulnerability 
+def search_for_cve_by_cve(cpe):
+    # Call data cleanse
+    cves = []
+        
+    for x in cve_cpe.select().join(nvd_db)   .where(cve_cpe.cpeid == cpe):
+
+        print x.cveid_id
+        cves.append(x.cveid_id)
+    return cves
+            
+
+# Takes CVE and gives CPEs - Vulnerability tied to multiple products
+def search_for_cpe_by_cve(cve):
+    cpes = []
+    cves = cve
+    
+    for x in cve_cpe.select(cve_cpe.cpeid).join(nvd_db).where(nvd_db.cveid == cve):
+        print x.cpeid
+        cpes.append(x.cpeid)
+    return cpes
+
+
+
+# Takes in CVEs and gives CWEs associated with CVEs 
+def search_for_cwe_by_cve(cve):
+    cves = []
+    cwes = []
+
+    for x in cve_cwe.select(cve_cwe.cweid).join(nvd_db).where(nvd_db.cveid ==cve):  
+        print x.cweid
+        cwes.append(x.cweid)
+    return(cwes)
+
+
+        
+# Takes CVEs and gives Exploit DB ID associated with the CVEs
+def search_for_exploit_by_cve(cve):
+    cves = []
+    expid = []
+    
+    for x in map_cve_exploitdb.select(map_cve_exploitdb.exploitdbid).join(nvd_db).where(nvd_db.cveid == cve):
+        
+        print x.exploitdbid
+        expid.append(x.exploitdbid)
+    return expid
+        
+
+if __name__ =='__main__':
+    processInput(userInput)
+
+
+
+
+
+
+
